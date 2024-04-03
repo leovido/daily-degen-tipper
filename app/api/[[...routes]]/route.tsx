a5c331e9d434ea2ee92be03343fec15db2458577
Christian Ray Leovido <18484997+leovido@users.noreply.github.com>
2024-04-03 22:44:13 +0100

    fix: add link to share

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -150,9 +150,10 @@ app.frame('/', async (c) => {
     ),
     intents: [
       frameData === undefined && <Button value="check">Check</Button>,
-      frameData !== undefined && <Button.Link href="https://warpcast.com/leovido">Made by @leovido</Button.Link>,
       frameData !== undefined && groupedArray.length > 5 && <Button value="dec">←</Button>,
       frameData !== undefined && groupedArray.length > 5 && <Button value="inc">→</Button>,
+      frameData !== undefined && <Button.Link href="https://warpcast.com/leovido">Made by @leovido</Button.Link>,
+      frameData !== undefined && <Button.Link href="https://warpcast.com/~/compose?text=Check%20who%20you%20tipped%20today%0A%0A&embeds[]=https://daily-degen-tipper.vercel.app/api">Share frame</Button.Link>,
     ],
   })
 })
